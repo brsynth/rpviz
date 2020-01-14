4140090bc496566264e22193e73c69b3a4e87d2c
Thomas Duigou <thomas.duigou@inra.fr>
2020-01-14 19:15:46 +0100

    makes SVG depiction of chemicals

--- rpviz/utils.py
+++ rpviz/utils.py
@@ -354,6 +354,41 @@ def annotate_cofactors(network, cofactor_file):
     return network
 
 
+def annotate_chemical_svg(network):
+    """Annotate chemical nodes with SVGs depiction.
+
+    :param network: dict, network of elements as outputted by the sbml_to_json method
+    :return: dict, network annotated
+    """
+    from rdkit.Chem import MolFromInchi
+    from rdkit.Chem.Draw import rdMolDraw2D
+    from rdkit.Chem.AllChem import Compute2DCoords
+    from urllib import parse
+
+    for node in network['elements']['nodes']:
+        if node['data']['type'] == 'chemical' and node['data']['inchi'] is not None:
+            inchi = node['data']['inchi']
+            try:
+                print(inchi)
+                mol = MolFromInchi(inchi)
+                if mol is None:
+                    raise BaseException('Mol is None')
+                Compute2DCoords(mol)
+                drawer = rdMolDraw2D.MolDraw2DSVG(200, 200)
+                drawer.DrawMolecule(mol)
+                drawer.FinishDrawing()
+                svg_draft = drawer.GetDrawingText().replace("svg:", "")
+                svg = 'data:image/svg+xml;charset=utf-8,' + parse.quote(svg_draft)
+                node['data']['svg'] = svg
+            except BaseException as e:
+                msg = 'SVG depiction failed from inchi: {}'.format(inchi)
+                logging.warning(msg)
+                logging.warning(e)
+                node['data']['svg'] = None
+
+    return network
+
+
 if __name__ == '__main__':
 
     pass
