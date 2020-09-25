035815128f62bd0f0a11b3075c207924d7f4cdf9
Thomas Duigou <thomas.duigou@inrae.fr>
2020-09-25 16:41:03 +0200

    feat: make rpviz installable as a package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='rpviz',
+    version=1.0,
+    description='Visualize pathways from the RetroPath Suite',
+    license='MIT',
+    author='Thomas Duigou, Melchior du Lac',
+    author_email='thomas.duigou@inrae.fr',
+    packages=['rpviz'],
+    keywords='rpviz',
+    classifiers=[
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
