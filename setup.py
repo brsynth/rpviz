f45ba3451ea4eee46cc7d6245ee472afa7f108ba
Thomas Duigou <thomas.duigou@inrae.fr>
2021-04-19 15:50:03 +0200

    build(setup.py): refresh info

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from setuptools import setup
 
 setup(
     name='rpviz',
-    version=1.0,
-    description='Visualize pathways from the RetroPath Suite',
+    version=0.1.0,
+    description='Visualize pathways from the RetroPath Suite.',
     license='MIT',
     author='Thomas Duigou, Melchior du Lac',
     author_email='thomas.duigou@inrae.fr',
-    packages=['rpviz'],
-    keywords='rpviz',
+    packages=find_packages(),
+    keywords=['rpviz'],
+    url='https://github.com/brsynth/rpreactor',
     classifiers=[
         'Topic :: Scientific/Engineering',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ]
 )
