ff944e5d0c4f9b8988ac2ef468f0a67fff940c17
Thomas Duigou <thomas.duigou@inrae.fr>
2021-05-25 16:25:57 +0200

    build: new release

--- RELEASE.md
+++ RELEASE.md
@@ -1,5 +1,10 @@
 # Release history
 
+## 0.1.1
+- feat: accept both folder and tarfile as input
+- tests: input tarfile example
+- docs(README): update
+
 ## 0.1.0
 - feat: update code according to the new rpSBML interface
 - feat: update json specifications
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rpviz',
-    version='0.1.0',
+    version='0.1.1',
     description='Visualize pathways from the RetroPath Suite.',
     license='MIT',
     author='Thomas Duigou, Melchior du Lac',
