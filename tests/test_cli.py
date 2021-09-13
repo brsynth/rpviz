9cac27d39ef4600432d6925b5a1f460c70c5b173
Thomas Duigou <thomas.duigou@inrae.fr>
2021-09-13 16:46:32 +0200

    tests: output ref file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,8 +3,17 @@ from pathlib import Path
 from rpviz import cli
 
 
-IN_DIR = Path(__file__).resolve().parent / 'inputs' / 'as_dir'
-IN_TAR = Path(__file__).resolve().parent / 'inputs' / 'as_tar.tgz'
+REF_IN_DIR = Path(__file__).resolve().parent / 'inputs' / 'as_dir'
+REF_IN_TAR = Path(__file__).resolve().parent / 'inputs' / 'as_tar.tgz'
+REF_OUT_DIR = Path(__file__).resolve().parent / 'outputs'
+
+
+def __read_and_sort(
+    path: str
+    ) -> list:
+    with open(path) as fh:
+        lines = [_ for _ in fh.readline()]
+    return lines.sort()
 
 
 def test_build_arg_parser(mocker):
@@ -18,17 +27,23 @@ def test_build_arg_parser(mocker):
 
 def test_dir_input(mocker, tmpdir):
     out_dir = tmpdir / 'odir'
-    args = ['prog', str(IN_DIR), str(out_dir)]
+    args = ['prog', str(REF_IN_DIR), str(out_dir)]
     mocker.patch('sys.argv', args)
     parser = cli.__build_arg_parser()
     args = parser.parse_args()
     cli.__run(args)
+    ref_file = REF_OUT_DIR / 'network.json'
+    test_file = out_dir / 'network.json'
+    assert __read_and_sort(test_file) == __read_and_sort(ref_file)
 
 
 def test_tar_input(mocker, tmpdir):
     out_dir = tmpdir / 'odir'
-    args = ['prog', str(IN_TAR), str(out_dir)]
+    args = ['prog', str(REF_IN_TAR), str(out_dir)]
     mocker.patch('sys.argv', args)
     parser = cli.__build_arg_parser()
     args = parser.parse_args()
     cli.__run(args)
+    ref_file = REF_OUT_DIR / 'network.json'
+    test_file = out_dir / 'network.json'
+    assert __read_and_sort(test_file) == __read_and_sort(ref_file)
